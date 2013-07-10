2156498f351faa0cbeea58dfb2a1f3f9e78c0517
Manu Achucarro <manuel.achucarro@gmail.com>
2013-07-10 00:27:10 +0000

    buffer encoding fixed

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var 	express = require('express'),
 		app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var message = fs.readFileSync('index.html', {encoding: 'utf8'});
+  var message = fs.readFileSync('index.html', 'utf8');
   response.send(message);
 });
 
